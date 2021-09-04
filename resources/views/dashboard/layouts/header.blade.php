763dbb6feedc7d272730dd424896c2b766b43041
THiNK <the.ashborn99@gmail.com>
2021-09-05 05:57:29 +0800

    Mencoba menggunakan feather icons pada tampilan dashboard

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -9,7 +9,7 @@
         <form action="/logout" method="POST">
             @csrf
             <button type="submit" class="nav-link px-3 bg-dark border-0">
-                <i class="bi bi-box-arrow-right"></i> Logout
+                Logout <span data-feather="log-out"></span>
             </button>
         </form>
         </div>
