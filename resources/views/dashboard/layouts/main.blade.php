6fb0d90a6ada35924cd6415558f15b009c9bb568
THiNK <the.ashborn99@gmail.com>
2021-09-05 20:26:49 +0800

    Menampilkan data post berdasarkan user yang login

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -18,8 +18,11 @@
     <div class="container-fluid">
         <div class="row">
             @include('dashboard.layouts.sidebar')
-
-            @yield('container')
+            
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                @yield('container')
+            </main>
+            
         </div>
     </div>
 
