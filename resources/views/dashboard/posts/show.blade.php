b4fee3f52404e0d9f0e8f926e1bd0fd331caf3f2
THiNK <the.ashborn99@gmail.com>
2021-09-09 18:01:27 +0800

    merubah link href untuk edit post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -8,7 +8,7 @@
                 <h2>{{ $post->title }}</h2>
                 
                 <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all my posts</a>
-                <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
                     <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
                         @method('delete')
                         @csrf
